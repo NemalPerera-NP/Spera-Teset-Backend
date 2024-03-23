5f4b109910e55bcb94db57acac6f2e1fd4224c3d
NemalPerera-NP <nemalperera1999@gmail.com>
2024-03-23 13:34:21 +0530

    implemented autogenerating swagger Payload Structure for API's in a JSON file

--- src/server.js
+++ src/server.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const cryptoRoutes = require("./routes/cryptoRotes");
-const userRoutes = require("./routes/UserRoutes");
 const connectDB = require("./config/db");
 
 const swaggerUi = require("swagger-ui-express");
-const swaggerJsdoc = require("swagger-jsdoc");
-// const swaggerDocument = require("../swagger_output.json");
+// const swaggerJsdoc = require("swagger-jsdoc");
+const swaggerDocument = require("../swagger_output.json");
 
 //importing controllers
 const {
@@ -22,37 +20,38 @@ const app = express();
 //define a port for the server
 const PORT = process.env.PORT || 8080;
 
-//swagger
-const options = {
-  definition: {
-    openapi: "3.0.0",
-    info: {
-      title: "Crypto Price Site API",
-      version: "1.0.0",
-    },
-    servers: [
-      {
-        url: `http://localhost:${PORT}/`,
-      },
-    ],
-  },
-  apis: ["./server.js"],
-};
-
-const swaggerSpec = swaggerJsdoc(options);
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-// app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 //url - http://localhost:8080/api-docs
 
 //to correctly handle JSON payloads in the HTTP request body (common in API requests), use the express.json() middleware.
 app.use(express.json());
 
-app.post("/api/auth/signup", userRegisterControler);
-app.post("/api/auth/login", loginControl);
-
 app.get("/", (req, res) => {
   res.send("Initial codeing on crypto project!");
 });
+
+app.post("/api/auth/signup", userRegisterControler);
+app.post("/api/auth/login", loginControl);
+
+//swagger
+// const options = {
+//   definition: {
+//     openapi: "3.0.0",
+//     info: {
+//       title: "Crypto Price Site API",
+//       version: "1.0.0",
+//     },
+//     servers: [
+//       {
+//         url: `http://localhost:${PORT}/`,
+//       },
+//     ],
+//   },
+//   apis: ["./server.js"],
+// };
+
+// const swaggerSpec = swaggerJsdoc(options);
+// app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
--- src/swaggerAutogen.js
+++ src/swaggerAutogen.js
@@ -1,27 +1,27 @@
-// const swaggerAutogen = require("swagger-autogen");
-// const dotenv = require("dotenv");
-// dotenv.config();
+const swaggerAutogen = require("swagger-autogen");
+const dotenv = require("dotenv");
+dotenv.config();
 
-// //PORT
-// const PORT = process.env.PORT || 8080;
-// const outputFile = "../swagger_output.json"; // The output file for the Swagger documentation
-// const endpointsFiles = ["./routes/cryptoRotes", "./routes/UserRoutes"]; // Paths to the files containing your route definitions
+//PORT
+const PORT = process.env.PORT || 8080;
+const outputFile = "../swagger_output.json"; // The output file for the Swagger documentation
+const endpointsFiles = ["./server.js", "./routes/*.js"]; // Paths to the files containing your route definitions
 
-// const doc = {
-//   openapi: "3.0.0",
-//   info: {
-//     title: "Crypto Price Site API",
-//     description: "API project for Spera Labs",
-//     version: "1.0.0"
-//   },
-//   servers: [
-//     {
-//       url: `http://localhost:${PORT}/`,
-//       description: "Development server"
-//     },
-//   ]
-// };
+const doc = {
+  openapi: "2.0.0",
+  info: {
+    title: "Crypto Price Site API",
+    description: "API project for Spera Labs",
+    version: "1.0.0",
+  },
+  servers: [
+    {
+      url: `http://localhost:${PORT}/`,
+      description: "Development server",
+    },
+  ],
+};
 
-// swaggerAutogen(outputFile, endpointsFiles, doc).then(() => {
-//   console.log("Swagger documentation generated!");
-// });
+swaggerAutogen(outputFile, endpointsFiles, doc).then(() => {
+  console.log("Swagger documentation generated!");
+});
