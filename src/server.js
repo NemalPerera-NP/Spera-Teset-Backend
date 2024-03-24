f61b38a860b7887882cb24c2de6867ebc271795a
NemalPerera-NP <nemalperera1999@gmail.com>
2024-03-24 11:44:48 +0530

    automated crypto price fetching method to fetch price every 5 miniutes when the server is launched

--- src/server.js
+++ src/server.js
@@ -9,7 +9,8 @@ const {
   userRegisterControler,
   loginControl,
 } = require("./controllers/UserControlers");
-const {saveCryptoPriceController} = require("./controllers/cryptoController");
+const { saveCryptoPriceController } = require("./controllers/cryptoController");
+const { getCryptoPrice } = require("./services/cryptoService");
 
 dotenv.config();
 connectDB(); // Connect to MongoDB
@@ -17,8 +18,17 @@ connectDB(); // Connect to MongoDB
 const app = express();
 const PORT = process.env.PORT || 8080; //define a port for the server
 
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-//url - http://localhost:8080/api-docs
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument)); //url - http://localhost:8080/api-docs
+
+setInterval(() => {
+  getCryptoPrice().catch((error) =>
+    console.error("Error updating crypto prices:", error)
+  );
+}, 300000); //fetch crypto price every 5 (ms-300000) minutes and save it in the DB
 
 //to correctly handle JSON payloads in the HTTP request body (common in API requests), use the express.json() middleware.
 app.use(express.json());
@@ -32,15 +42,7 @@ app.post("/api/auth/login", loginControl);
 
 //GET
 // Protected route for fetching cryptocurrency price,
-app.get("/api/crypto/price/:id", authenticateToken, saveCryptoPriceController);
-
-
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
-
-
-
+// app.get("/api/crypto/price/:id", authenticateToken, saveCryptoPriceController);
 
 //swagger
 // const options = {
--- src/services/cryptoService.js
+++ src/services/cryptoService.js
@@ -8,16 +8,11 @@ const CryptoPrice = require("../models/CryptoPriceModel");
 const BASE_URL = "https://api.coingecko.com/api/v3";
 
 //cryptoId = "bitcoin"
-const getCryptoPrice = async () => {
+const getCryptoPrice = async (
+  cryptoIds = ["bitcoin", "ethereum", "ripple", "cardano", "solana", "dogecoin"]
+) => {
   try {
-    const ids = [
-      "bitcoin",
-      "ethereum",
-      "ripple",
-      "cardano",
-      "solana",
-      "dogecoin",
-    ].join(",");
+    const ids = cryptoIds.join(",");
 
     const response = await axios.get(`${BASE_URL}/simple/price`, {
       params: {
@@ -26,26 +21,36 @@ const getCryptoPrice = async () => {
       },
     });
 
-    // Iterate over each cryptocurrency and insert a new document in the database
     const cryptoPriceList = Object.entries(response.data).map(([id, data]) => {
-      const cryptoPrices = new CryptoPrice({
+      return new CryptoPrice({
         cryptoId: id,
         price: data.usd,
         updatedDate: new Date(),
-      });
-      return cryptoPrices.save(); // Save the new document
+      }).save();
     });
 
-    // Wait for all save operations to complete
-    console.log("priceSavePromises..........", cryptoPriceList);
-
     await Promise.all(cryptoPriceList);
-    console.log("response.data[cryptoId].usd>>>>>>>>", response.data);
-    return response.data;
+    console.log("Crypto prices updated successfully");
+
+    // Iterate over each cryptocurrency and insert a new document in the database
+    // const cryptoPriceList = Object.entries(response.data).map(([id, data]) => {
+    //   const cryptoPrices = new CryptoPrice({
+    //     cryptoId: id,
+    //     price: data.usd,
+    //     updatedDate: new Date(),
+    //   });
+    //   return cryptoPrices.save(); // Save the new document
+    // });
+
+    // Wait for all save operations to complete
+    // console.log("priceSavePromises..........", cryptoPriceList);
+    // await Promise.all(cryptoPriceList);
+    // console.log("response.data[cryptoId].usd>>>>>>>>", response.data);
+    //return response.data;
 
     //return response.data[cryptoId].usd;
   } catch (error) {
-    console.error(`Failed to fetch price for ${cryptoId}: ${error.message}`);
+    console.error(`Failed to fetch prices: ${error.message}`);
     throw error;
   }
 };
